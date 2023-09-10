d3e20f8276cba0caaf1eee001f52e649aab33200
vendredl <node.js_@kakao.com>
2023-09-10 21:51:04 +0900

    :lipstick: 다크모드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { BrowserRouter as Router, Route, BrowserRouter, Routes } from 'react-router-dom';
 import CharacterDetailPage from './pages/CharacterDetailPage';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import About from './pages/AboutPage';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './utils/themes';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { darkMode } from './utils/atom';
+
 
 function App() {
+
+  const darkmode = useRecoilValue(darkMode);
+
+  console.log(darkmode ? darkTheme : lightTheme)
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" Component={HomePage}/>
-        <Route path="/character/:id" Component={CharacterDetailPage}></Route>
-        <Route path="/about" Component={About}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ThemeProvider theme={darkmode ? darkTheme : lightTheme}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" Component={HomePage}/>
+            <Route path="/character/:id" Component={CharacterDetailPage}></Route>
+            <Route path="/about" Component={About}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 }
 
 export default App;
+
+
