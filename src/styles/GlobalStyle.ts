6516c50c9842ea86806ded8f2ceba8f02ccda126
vendredl <node.js_@kakao.com>
2023-09-11 01:10:38 +0900

    :lipstick: 마우스 커서 커스텀

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,7 +1,10 @@
 import {createGlobalStyle} from 'styled-components';
 
+
 export const GlobalStyle = createGlobalStyle`
     body {
         background-color: ${props => props.theme.background};
+        cursor: url('../icons8-marvel-48.png'), auto;
     }
-`
+`;
+
