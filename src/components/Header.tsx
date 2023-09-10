049591db7989eabd3f355b8142e3829837405291
vendredl <node.js_@kakao.com>
2023-09-10 22:10:15 +0900

    :lipstick: 다크모드 버튼 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
+import { darkMode } from '../utils/atom';
+import { MdLightMode, MdModeNight } from "react-icons/md";
 
 const GlobalStyle = createGlobalStyle`
     body {
         font-family: 'Montserrat Alternates', sans-serif;
     }
 `
+
 
 const HeaderContainer = styled.header`
   background-color: #282c34;
@@ -42,7 +46,24 @@ const MenuItem = styled(Link)`
   }
 `;
 
+const DarkModeBtn = styled.div`
+    color: ${(props) => props.theme.text};
+    background: ${(props) => props.theme.body};
+    font-size: 1.5rem;
+    border-radius: 100%;
+    width: 35px;
+    height: 35px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+
+
 function Header() {
+
+    const [darkmode, setDarkMode] = useRecoilState(darkMode);
+
   return (
     <>
     <GlobalStyle />
@@ -51,6 +72,9 @@ function Header() {
       <Menu>
         <MenuItem to="/">Home</MenuItem>
         <MenuItem to="/about">About</MenuItem>
+        <DarkModeBtn onClick={() => setDarkMode(!darkmode)} >
+            {darkmode ? <MdLightMode /> : <MdModeNight />}
+        </DarkModeBtn>
       </Menu>
     </HeaderContainer>
     </>
